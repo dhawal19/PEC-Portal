44809bbdd13b347fb0785e346637a3f49876ace5
Chirag <chiraggarg.red@gmail.com>
2024-02-15 23:15:04 +0530

    not found page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
+          <Route index element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
