89f40956664a74558b52afa7103ad4c9698ac37a
Chirag <chiraggarg.red@gmail.com>
2024-02-20 21:37:48 +0530

    home page

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 
 const UserProfile = ({ user }) => {
     return (
-        <div className="bg-slate-950 p-4 w-1/5 h-full fixed top-0 left-0 overflow-y-auto">
-            <div className="flex items-center mb-4">
-                <img src={user.picture} alt="Profile" className="w-12 h-12 rounded-full mr-2" />
+        <div className="bg-gray-800 p-4 w-1/5 h-full fixed top-0 left-0 overflow-y-auto">
+            <div className="flex items-center justify-center mb-4">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTazqf7Ow72BkH5xm0t8vfXx-OPgNBYqjYYKQ&usqp=CAU" alt="Profile" className="w-20 h-20" />
             </div>
-            <div>
+            <div className='mt-5'>
                 <h2 className="text-lg font-semibold text-slate-300">{user.name}</h2>
-                <p className="text-sm  text-slate-300">{user.email}</p>
+                <p className="text-sm  text-slate-300 mt-2">{user.email}</p>
             </div>
-            <p className="text-sm text-slate-300 mb-2">{user.bio}</p>
+            <p className="text-sm text-slate-300 mb-2 mt-3">{user.bio}</p>
             <div className="mb-4">
-                <p className="text-sm font-semibold mb-1 text-slate-400">Number of Connections:</p>
+                <p className="text-sm font-semibold mb-1 text-slate-400 mt-3">Number of Connections:</p>
                 <p className="text-sm text-slate-300">{user.connections}</p>
             </div>
             <div className="mb-4">
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,9 +16,25 @@ export default function HomePage() {
             <div className='w-1/5 h-full'>
                 <UserProfile user={user} />
             </div>
-            <div className='w-4/5 bg-slate-900 h-full'>
-                <h1 className="mt-20 text-center font-semibold text-6xl text-slate-300">Hello, Welcome User</h1>
-                <HomeCard />
+            <div className='w-4/5 bg-gray-700 h-full'>
+                <h1 className="mt-20 text-center font-semibold text-6xl text-slate-300">Hello, Welcome {user.name}</h1>
+                <h3 className="mt-10 text-center font-semibold text-3xl text-slate-300">Here are a few things you can do on PEC-Connect</h3>
+                <div className='mt-12 flex justify-center'>
+                    <div className='mx-4 flex items-center'>
+                        <HomeCard title={'Attendance'} />
+                    </div>
+                    <div className='mx-4 flex items-center'>
+                        <HomeCard title={'Time Table'} />
+                    </div>
+                </div>
+                <div className='mt-10 flex justify-center'>
+                    <div className='mx-4 flex items-center'>
+                        <HomeCard title={'Connect'} />
+                    </div>
+                    <div className='mx-4 flex items-center'>
+                        <HomeCard title={'Courses'} />
+                    </div>
+                </div>
             </div>
         </div>
     )
