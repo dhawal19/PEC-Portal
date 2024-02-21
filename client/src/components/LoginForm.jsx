908a72775bc7b535980d018d654f8533e04cc04b
Chirag <chiraggarg.red@gmail.com>
2024-02-21 21:30:22 +0530

    login page edited

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,8 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { loginUser } from '../features/auth/authQueries';
 import { useDispatch } from 'react-redux'
 import { setUser, setToken, selectUser } from '../features/auth/authSlice';
-import { useState } from 'react'; 
-import { useQueryClient, useMutation} from "react-query";
+import { useState } from 'react';
+import { useQueryClient, useMutation } from "react-query";
 import { useSelector } from 'react-redux';
 
 
@@ -38,69 +38,69 @@ const LoginForm = () => {
     loginMutation.mutate({ email, password });
   };
 
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 ">
-        <div className="absolute bg-gradient-to-r from-green-400 via-teal-400 to-blue-400 shadow-md rounded-xl pt-6 pb-8 flex h-auto w-8/12">
-        
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
+      <div className="bg-black shadow-md rounded-xl pt-6 pb-8 flex h-auto w-8/12 border-2 border-white">
+
         {/*Close button*/}
         <button
-        className="absolute top-0 right-0 my-2 pb-3 mr-2 bg-transparent border-0 rounded-full h-8 w-8 text-gray-600 hover:text-gray-800 text-xl hover:bg-gray-300 cursor-pointer focus:outline-none focus:shadow-outline"
-        onClick={() => console.log('Close button clicked')}
-      > {/* Add onClick event to close the form */}
-        &times;
-      </button>
+          className="absolute top-0 right-0 my-2 pb-3 mr-2 bg-transparent border-0 rounded-full h-8 w-8 text-gray-600 hover:text-gray-800 text-xl hover:bg-gray-300 cursor-pointer focus:outline-none focus:shadow-outline"
+          onClick={() => console.log('Close button clicked')}
+        > {/* Add onClick event to close the form */}
+          &times;
+        </button>
         <div className="flex flex-start justify-center w-6/12">
-        
-      {/*Form Container*/}
-      <div className='flex-col w-7/12'>
-          <h1 className="text-start text-3xl font-bold mb-4">Login</h1>
-          <p className="text-start text-gray-700 text-s mb-4">
+
+          {/*Form Container*/}
+          <div className='flex-col w-7/12'>
+            <h1 className="text-start text-3xl font-bold mb-4">Login</h1>
+            <p className="text-start text-white text-s mb-4">
               Welcome back! Sign in to your account
-          </p>
-        <form action='' onSubmit={handleLogin}>
-            <div className="mb-4 text-l mt-8 text-start">
-              <input
-                className="shadow appearance-none border rounded-3xl w-full ps-3 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4 mt-4 text-l">
-              <input
-                className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {loginMutation.isError && (<p className="text-sm text-red-700">
-                Login failed! Please check your credentials and try again.
+            </p>
+            <form action='' onSubmit={handleLogin}>
+              <div className="mb-4 text-l mt-8 text-start">
+                <input
+                  className="shadow appearance-none border rounded-3xl w-full ps-3 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-4 mt-4 text-l">
+                <input
+                  className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {loginMutation.isError && (<p className="text-sm text-red-700">
+                  Login failed! Please check your credentials and try again.
                 </p>)}
-              <a>
-                  <p className="text-right text-sm text-teal-700 hover:text-blue-800">
-                      Forgot Password?
+                <a>
+                  <p className="text-right text-sm text-teal-600 hover:text-blue-600">
+                    Forgot Password?
                   </p>
-              </a>
-            </div>
-            
-            <div className="flex justify-center">
-              <button
-                className="bg-blue-500 hover:bg-green-700 text-white py-2 px-3 rounded-3xl w-full focus:outline-none hover:scale-105 duration-300  focus:shadow-outline"
-                type="submit"
-              >
-                {loginMutation.isLoading ? 'Logging in...' : 'Login'}
-              </button>
-            </div>
-          </form>
-          {/*Social Media Login*/}
-          <div className="flex items-center mt-4 mb-4">
+                </a>
+              </div>
+
+              <div className="flex justify-center">
+                <button
+                  className="bg-blue-500 hover:bg-green-700 text-white py-2 px-3 rounded-3xl w-full focus:outline-none hover:scale-105 duration-300  focus:shadow-outline"
+                  type="submit"
+                >
+                  {loginMutation.isLoading ? 'Logging in...' : 'Login'}
+                </button>
+              </div>
+            </form>
+            {/*Social Media Login*/}
+            <div className="flex items-center mt-4 mb-4">
               <div className="w-full border-t border-blue-700"></div>
               <div className="px-3">
-                <span className="text-gray-700">OR</span>
+                <span className="text-white">OR</span>
               </div>
               <div className="w-full border-t border-blue-700"></div>
             </div>
@@ -120,28 +120,29 @@ const LoginForm = () => {
                 Login with Github
               </button>
             </div>
+          </div>
+
         </div>
-        
+        {/*Vertical line*/}
+        <div className=' border-2 border-white border-opacity-30 w-0.5 rotate-180'></div>
+        {/*Image Container*/}
+        <div className='flex flex-col w-5/12'>
+          {/*Image*/}
+          <div className="flex justify-center mt-6">
+            <h3 className="text-white text-md my-6 "> New to our website?</h3>
+          </div>
+          <div className="flex justify-center">
+            <Link to='/register' className='flex justify-center bg-blue-500 hover:bg-green-700 text-white py-2 px-3 rounded-3xl w-8/12 focus:outline-none focus:shadow-outline hover:scale-105 duration-300'>
+              <button >
+                Register
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-      {/*Vertical line*/}
-      <div className=' border-l border-gray-600 border-opacity-30 w-0.5 rotate-180'></div> 
-            {/*Image Container*/}
-            <div className='flex flex-col w-5/12'>
-                {/*Image*/}
-                <div className="flex justify-center mt-6">
-                    <h3 className="text-gray-700 text-md my-6 "> New to our website?</h3>
-                </div>
-                <div className="flex justify-center">
-                    <Link to='/register' className= 'flex justify-center bg-blue-500 hover:bg-green-700 text-white py-2 px-3 rounded-3xl w-8/12 focus:outline-none focus:shadow-outline hover:scale-105 duration-300'>
-                   <button >
-                        Register
-                    </button>
-                    </Link>
-                </div>
-            </div>
     </div>
-  </div>
-)};
+  )
+};
 
 
 export default LoginForm;
